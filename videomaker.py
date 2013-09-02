424aaaa9d88056c3b743ba74e9525fd6dedeb787
François Boulogne <fboulogne sciunto org>
2013-09-02 19:55:38 +0200

    remove old code

--- videomaker.py
+++ videomaker.py
@@ -104,20 +104,6 @@ def alphanum_key(s):
 ##############Natural sorting : end
 
 
-#class VideoSection():
-#    """
-#    Gather data about a video section
-#
-#
-#    :repeat: repeat the movie N times
-#    """
-#    def __init__(self, path, num_frame_slide, number=1, repeat=1):
-#        self.path = path
-#        self.num_frame_slide = num_frame_slide
-#        self.number = number
-#        self.repeat = repeat
-
-
 def make_slide(tex_path, tmp_path, resolution=(1200, 800)):
     """
     Make introduction png files
@@ -147,89 +133,6 @@ def make_slide(tex_path, tmp_path, resolution=(1200, 800)):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return pngfile
-
-
-#def prepare_pictures(output_dir, opening, bodies, ending, resolution=(800, 600), tmp_loc=None):
-#    """
-#    Put pictures in tmp_path with a correct name (sorted)
-#
-#    :param output_dir: path where final images are written before making the video
-#    :param opening: :class:`VideoSection` instance for opening
-#    :param bodies: :class:`VideoSection` instance for bodies
-#    :param ending: :class:`VideoSection` instance for ending
-#    :param resolution: Tuple speciafying the resolution
-#    :param tmp_loc: path where tmp dirs are created
-#    """
-#    logger.info('Prepare pictures...')
-#    # Start a generator
-#    gen = name_it(output_dir)
-#    # Background: auto-generated for the moment
-#    bg = Image.new("RGB", resolution, color=(0, 0, 0)) # Black.
-#    # Angle to rotate each image (for futher improvements)
-#    angle = 0
-#
-#    #Part 1, opening
-#    if opening.path:
-#        logger.info('[Opening]')
-#        tmp_path = tempfile.mkdtemp(dir=tmp_loc, prefix='tmp_open')
-#        introfile = make_slide(opening.path, tmp_path, resolution)
-#        if introfile:
-#            for count in range(opening.num_frame_slide):
-#                shutil.copy(introfile, gen.__next__())
-#        shutil.rmtree(tmp_path)
-#
-#    #Part 2, body
-#    logger.info('[Body]')
-#    for body in bodies:
-#        tmp_path = tempfile.mkdtemp(dir=tmp_loc, prefix='tmp_body')
-#        slide = make_slide(body.path, tmp_path, resolution)
-#        if slide:
-#            for count in range(body.num_frame_slide):
-#                shutil.copy(slide, gen.__next__())
-#        shutil.rmtree(tmp_path)
-#
-#        pictures = sorted(os.listdir(body.path), key=alphanum_key)
-#        pictures = [os.path.join(body.path, item) for item in pictures]
-#
-#        tmp_file = tempfile.mkstemp(dir=tmp_loc, prefix='pic')
-#        if body.number >= 1:
-#            # duplicate the picture...
-#            times = math.floor(body.number)
-#            for rep in range(repeat):
-#                for item in pictures:
-#                    logger.debug('Process: %s' % item)
-#                    # stick the item on a background
-#                    full_im = Image.open(item)
-#                    full_im = add_bg(full_im, bg, angle=angle)
-#                    full_im.save(tmp_file)
-#                    for time in range(times):
-#                        shutil.copy(tmp_file, gen.__next__())
-#        elif body.number < 1:
-#            # pick one every...
-#            every = math.floor(1/body.number)
-#            for rep in range(repeat):
-#                for item in pictures[::every]:
-#                    logger.debug('Process: %s' % item)
-#                    # stick the item on a background
-#                    full_im = Image.open(item)
-#                    full_im = add_bg(full_im, bg, angle=angle)
-#                    full_im.save(tmp_file)
-#                    shutil.copy(tmp_file, gen.__next__())
-#        shutil.rmtree(tmp_path)
-#
-#    #Part 3, ending
-#    if ending.path:
-#        logger.info('[Ending]')
-#        tmp_path = tempfile.mkdtemp(dir=tmp_loc, prefix='tmp_end')
-#        endfile = make_slide(ending.path, tmp_path, resolution)
-#        if endfile:
-#            for count in range(ending.num_frame_slide):
-#                shutil.copy(endfile, gen.__next__())
-#        shutil.rmtree(tmp_path)
-
-
-
-
 
 
 class Video():
@@ -334,85 +237,6 @@ class Video():
         #Delete the self.pic_dir
         #logging.debug('Delete the tmp_dir %s' % tmp_path)
         #shutil.rmtree(tmp_path)
-
-
-#def old():
-#    parser = argparse.ArgumentParser(description='', epilog='')
-#    parser.add_argument('conf', help='Configuration file', metavar='CONF')
-#    parser.add_argument('-t', '--tmp', metavar='TMP',
-#                        default=None, help='Directery where are stored tmp files')
-#    parser.add_argument('-d', '--debug', action='store_true',
-#                        default=False, help='Run in debug mode')
-#
-#    args = parser.parse_args()
-#
-#    if args.debug:
-#        llevel = logging.DEBUG
-#    else:
-#        llevel = logging.INFO
-#    logger = logging.getLogger()
-#    logger.setLevel(llevel)
-#
-#    steam_handler = logging.StreamHandler()
-#    steam_handler.setLevel(llevel)
-#    logger.addHandler(steam_handler)
-#
-#    config = configparser.ConfigParser()
-#    config.read(args.conf)
-#
-#    cwd = os.getcwd()
-#    FPS = 25
-#    resolution = (800, 600)
-#
-#    #Movie section
-#    output = config['movie'].get('output', 'output')
-#
-#    #Opening
-#    open_duration = config['opening'].getint('duration', 0)  # seconds
-#    opening_section = VideoSection(config['opening'].get('path', None), FPS * open_duration)
-#
-#    #Body
-#    pic_paths = config['body'].get('path').split(',')
-#    inifps = config['body'].getint('inifps', 1)
-#    speed = config['body'].getint('speed', 1)
-#    repeat = config['body'].getint('repeat', 1)
-#
-#    number = FPS / inifps / speed
-#    body_sections = [VideoSection(path, config['body'].getint('duration', 0), number, repeat) for path in pic_paths]
-#
-#    #Ending
-#    end_duration = config['ending'].getint('duration', 0)  # seconds
-#    end_section = VideoSection(config['ending'].get('path', None), FPS * end_duration)
-#
-#    #Prepare pictures in tmp dir
-#    tmp_path = tempfile.mkdtemp(dir=args.tmp)
-#    logger.debug('tmp_path for pictures %s', tmp_path)
-#
-#    prepare_pictures(tmp_path, opening_section, body_sections, end_section,
-#                     resolution=resolution, tmp_loc=args.tmp)
-#
-#    #Encode the movie
-#    logger.info('Generate the movie...')
-#    os.chdir(tmp_path)
-#    command = ['mencoder', 'mf://*.png', '-mf', 'fps='+str(FPS),
-#               '-vf', 'scale=800:600',
-#               '-o', 'output.avi',
-#               '-ovc', 'xvid',
-#               '-xvidencopts', 'bitrate=500'
-#               ]
-#    logging.debug('command: ' + str(command))
-#    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#    stdout, stderr = process.communicate()
-#    logging.debug(stdout.decode('utf8'))
-#    logging.warning(stderr.decode('utf8'))
-#
-#    #Copy the movie
-#    logging.debug('Move to %s' % cwd)
-#    shutil.copy('output.avi', os.path.join(cwd, output + '.avi'))
-#    #Delete the tmp dir
-#    logging.debug('Delete the tmp_dir %s' % tmp_path)
-#    shutil.rmtree(tmp_path)
-
 
 
 if __name__ == '__main__':
